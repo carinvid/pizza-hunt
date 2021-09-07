af266338e2b402c6e4edf80e8caf4ba9853205a7
Carolina Reedy <carinvid@gmail.com>
2021-09-06 20:51:56 -0600

    adding routes

--- module-18-starter/routes/api/pizza-routes.js
+++ module-18-starter/routes/api/pizza-routes.js
@@ -6,4 +6,18 @@ router.route("/").get().post();
 // Set up GET one, PUT, and DELETE at /api/pizzas/:id
 router.route("/:id").get().put().delete();
 
+const {
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza,
+} = require("../../controllers/pizza-controller");
+
+// /api/pizzas
+router.route("/").get(getAllPizza).post(createPizza);
+
+// /api/pizzas/:id
+router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
+
 module.exports = router;
